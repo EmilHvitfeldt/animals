7f1f936445c4827f3c5e407c916782702ca17b7d
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2020-10-06 00:02:09 -0700

    add mean_weight variable

--- data-raw/animals.R
+++ data-raw/animals.R
@@ -3,6 +3,30 @@
 library(animals)
 library(dplyr)
 library(stringr)
+library(purrr)
+
+
+calc_mean <- function(text) {
+  if (is.na(text)) return(NA)
+
+  if (str_detect(text, "-")) {
+    numbers <- str_split(text, pattern = " +- +")[[1]]
+    res <- mean(purrr::map_dbl(numbers, convert2g))
+  } else {
+    res <- convert2g(text)
+  }
+
+  res / 1000
+}
+
+convert2g <- function(x) {
+  res <- readr::parse_number(x)
+
+  if (str_detect(x, "kg")) {
+    res <- res * 1000
+  }
+  res
+}
 
 animals <- animals_raw %>%
   mutate(diet = case_when(
@@ -13,6 +37,8 @@ animals <- animals_raw %>%
     str_detect(diet, "^Om") ~ "Omnivore",
     TRUE ~ "Unknown"
   )) %>%
-  select(text, diet, lifestyle)
+  mutate(mean_weight = str_remove(weight, "\\(+.*"),
+         mean_weight = map_dbl(mean_weight, calc_mean)) %>%
+  select(text, diet, lifestyle, mean_weight)
 
 usethis::use_data(animals, overwrite = TRUE)
